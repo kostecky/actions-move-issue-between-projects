dac28d01487d27daafbf34d37dede355aa8130f9
Kristian Kostecky <kris@vmfarms.com>
2022-01-27 11:13:48 -0500

    Rename status

--- move.sh
+++ move.sh
@@ -24,14 +24,14 @@ if [[ ! -f .org_settings ]]; then
   }' -f org=${GITHUB_ORGANIZATION} > .org_settings
 fi
 
-customer_pipeline_id=$(jq -r '.data.organization.projectsNext.nodes[] | select(.title=="Customer Pipeline")| .id ' .org_settings)
-scheduled_work_id=$(jq -r '.data.organization.projectsNext.nodes[] | select(.title=="Scheduled Work")| .id ' .org_settings)
-cp_approved_status_id=$(jq -r --arg CP_ID ${customer_pipeline_id} '.data.organization.projectsNext.nodes[] | select(.id==$CP_ID) | .fields.edges[] | select(.node.name=="Status") | .node.settings | fromjson.options[] | select(.name | contains("Approved")) | .id' .org_settings)
+client_pipeline_id=$(jq -r '.data.organization.projectsNext.nodes[] | select(.title=="Client Pipeline")| .id ' .org_settings)
+scheduled_tasks_id=$(jq -r '.data.organization.projectsNext.nodes[] | select(.title=="Scheduled Tasks")| .id ' .org_settings)
+cp_approved_status_id=$(jq -r --arg CP_ID ${client_pipeline_id} '.data.organization.projectsNext.nodes[] | select(.id==$CP_ID) | .fields.edges[] | select(.node.name=="Status") | .node.settings | fromjson.options[] | select(.name | contains("Approved")) | .id' .org_settings)
 
-if [[ ! -f .customer_pipeline_data ]]; then
+if [[ ! -f .client_pipeline_data ]]; then
   gh api graphql -f query='
-  query($customer_pipeline_id: ID!) {
-    node(id: $customer_pipeline_id) {
+  query($client_pipeline_id: ID!) {
+    node(id: $client_pipeline_id) {
       ... on ProjectNext {
         items(first: 100) {
           nodes{
@@ -54,10 +54,10 @@ if [[ ! -f .customer_pipeline_data ]]; then
         }
       }
     }
-  }' -f customer_pipeline_id=${customer_pipeline_id} > .customer_pipeline_data
+  }' -f client_pipeline_id=${client_pipeline_id} > .client_pipeline_data
 fi
 
-approved_issue_ids=$(jq -r --arg CP_STATUS_ID ${cp_approved_status_id} '.data.node.items.nodes[] | select(.fieldValues.nodes[].value==$CP_STATUS_ID) | [.id,.content.id] | join("@")' .customer_pipeline_data)
+approved_issue_ids=$(jq -r --arg CP_STATUS_ID ${cp_approved_status_id} '.data.node.items.nodes[] | select(.fieldValues.nodes[].value==$CP_STATUS_ID) | [.id,.content.id] | join("@")' .client_pipeline_data)
 
 for issue_ids in $approved_issue_ids; do
   echo "Working on ${issue_ids}"
@@ -68,8 +68,8 @@ for issue_ids in $approved_issue_ids; do
   echo $content_issue_id
   echo $item_issue_id
 
-  # Add issues to Scheduled Work Queue
-  echo "Adding ${content_issue_id} to Scheduled Work Queue"
+  # Add issues to Scheduled Tasks Queue
+  echo "Adding ${content_issue_id} to Scheduled Tasks Queue"
   gh api graphql -f query='
   mutation($project_id: ID!, $issue_id: ID!) {
     addProjectNextItem(input: {projectId: $project_id contentId: $issue_id}) {
@@ -77,10 +77,10 @@ for issue_ids in $approved_issue_ids; do
         id
       }
     }
-  }' -f project_id=${scheduled_work_id} -f issue_id=${content_issue_id}
+  }' -f project_id=${scheduled_tasks_id} -f issue_id=${content_issue_id}
 
   if [[ $? -ne 0 ]]; then
-    echo "Couldn't add Issue to Scheduled work, skipping..."
+    echo "Couldn't add Issue to Scheduled tasks, skipping..."
     continue
   fi
 
@@ -96,9 +96,9 @@ for issue_ids in $approved_issue_ids; do
     ) {
       deletedItemId
     }
-  }' -f project_id=${customer_pipeline_id} -f issue_id=${item_issue_id}
+  }' -f project_id=${client_pipeline_id} -f issue_id=${item_issue_id}
 done
 
 # Project boards changed, delete cached data for next run
-rm .customer_pipeline_data
+rm .client_pipeline_data
 
